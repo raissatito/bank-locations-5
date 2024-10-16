cbf40dc87ba14bfaf8912f1b5742706eb513211e
name <email>
2024-10-16 16:58:29 +0700

    add : create cabang

--- src/services/api/locations.js
+++ src/services/api/locations.js
@@ -5,24 +5,57 @@ export async function getAll(params) {
     return await prisma.location.findMany();
 }
 
-export async function getByID() {
-    // TODO
-    
+export async function getByID(id) {
+    if (typeof id !== "number") {
+        throw new Error("Invalid ID");
+    }
+
+    return await prisma.location.findUnique({
+        where: {
+            id: id,
+        },
+    });
 }
 
+export async function create(data) {
+    if (!data.location_name ||
+        !data.address ||
+        !data.province ||
+        !data.city ||
+        !data.latitude ||
+        !data.longitude ||
+        !data.type ||
+        !data.category
+    ) {
+        throw new Error("Missing required fields");
+    }
 
-export async function create() {
-    // TODO
-    
+    return await prisma.location.create({
+        data: {
+            location_name: data.location_name,
+            address: data.address,
+            province: data.province,
+            city: data.city,
+            latitude: parseFloat(data.latitude),
+            longitude: parseFloat(data.longitude),
+            type: data.type,
+            category: data.category,
+        },
+    });
 }
 
 export async function update() {
     // TODO
-    
 }
 
-export async function destroy() {
-    // TODO
-    
-}
+export async function destroy(id) {
+    if (typeof id !== "number") {
+        throw new Error("Invalid ID");
+    }
 
+    return await prisma.location.delete({
+        where: {
+            id: id,
+        },
+    });
+}
