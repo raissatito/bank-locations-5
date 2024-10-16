20f52cbef35f729969fc69e273264eeacf52ce11
Dimas Tri Mustakim <test31122001@gmail.com>
2024-10-16 15:13:07 +0700

    feat: adding find by id, and delete cabang

--- src/pages/api/locations/[id].js
+++ src/pages/api/locations/[id].js
@@ -1,14 +1,49 @@
+import { getByID, destroy } from "@/services/api/locations";
+
 export default async function handler(req, res) {
-  if (req.method == 'PUT') {
-    // TODO
-  }
+    const { id } = req.query;
+    const parsedId = parseInt(id, 10);
 
-  if (req.method == 'DELETE') {
-    // TODO
-  }
+    if (isNaN(parsedId)) {
+        res.status(400).json({
+            error: true,
+            message: "Invalid ID: Must be a number!",
+        });
 
-  if (req.method == 'GET') {
-    // TODO
-  }
-  res.status(450).json({error: true})
+        return;
+    }
+
+    if (req.method == "PUT") {
+        // TODO
+    }
+
+    if (req.method == "DELETE") {
+        const deleted = await destroy(parsedId)
+            .then((data) => {
+                res.status(200).json({ message: "Deleted", data: data });
+            })
+            .catch((error) => {
+                res.status(500).json({
+                    error: true,
+                    message: "Error deleting",
+                    debug: error,
+                });
+            });
+
+        return;
+    }
+
+    if (req.method == "GET") {
+        const data = await getByID(parsedId)
+            .then((data) => {
+                res.status(200).json({ data: data });
+            })
+            .catch((error) => {
+                res.status(500).json({ error: true, message: error.message });
+            });
+
+        return;
+    }
+
+    res.status(450).json({ error: true, message: "Method not Supported" });
 }
